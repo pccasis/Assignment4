f89e79a3c9182c1d42d5f8d75ec2c44888276f83
Plinks <plinks@gmail.com>
2023-07-04 23:26:35 -0400

    assignment 4

---
+++ server.js
@@ -0,0 +1,115 @@
+/*********************************************************************************
+*  WEB700 – Assignment 04
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Plinky Charmaine Asis Student ID: 129640223 Date: June 18, 2023
+*
+********************************************************************************/ 
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+var path = require("path");
+var collegeData = require("./modules/collegeData");
+const { countReset } = require("console");
+app.use(express.urlencoded({extended: true}));
+
+
+
+collegeData.initialize()
+    .then(() => {
+        app.get("/", (req, res) => {
+            res.sendFile(path.join(__dirname, "/views/home.html"));
+        });
+        
+        app.get("/students", (req, res) => {
+            collegeData.getAllStudents()
+                .then((students) => {
+                    if (req.query.course) {
+                        collegeData.getStudentsByCourse(req.query.course)
+                            .then((studentsByCourse) => {
+                                res.json(studentsByCourse);
+                            })
+                            .catch((error) => {
+                                res.status(404).json({ message: "no results" });
+                            });
+                    } else {
+                        res.json(students);
+                    }
+                })
+                .catch((error) => {
+                    res.status(404).json({ message: "no results" });
+                });
+        });
+
+        app.get('/students/add', (req, res)=>{
+            res.sendFile(__dirname + '/views/addStudent.html')
+        });
+
+        app.post("/students/add", (req, res)=>{
+            collegeData.addStudent(req.body)
+            .then(()=>{
+                res.redirect("/students")
+            })
+            .catch((error)=>{
+                console.error(error)
+                res.redirect('ERROR' + error);
+            })
+        })
+        
+        app.get("/tas", (req, res) => {
+            collegeData.getTAs()
+                .then((tas) => {
+                    res.json(tas);
+                })
+                .catch((error) => {
+                    res.status(404).json({ message: "no results" });
+                });
+        });
+        
+        app.get("/courses", (req, res) => {
+            collegeData.getCourses()
+                .then((courses) => {
+                    res.json(courses);
+                })
+                .catch((error) => {
+                    res.status(404).json({ message: "no results" });
+                });
+        });
+        
+        app.get("/students/:num", (req, res) => {
+            collegeData.getStudentByNum(req.params.num)
+                .then((student) => {
+                    res.json(student);
+                })
+                .catch((error) => {
+                    res.status(404).json({ message: "no results" });
+                });
+        });
+        
+        app.get("/about", (req, res) => {
+            res.sendFile(path.join(__dirname, "/views/about.html"));
+        });
+        
+        app.get("/htmlDemo", (req, res) => {
+            res.sendFile(path.join(__dirname, "/views/htmlDemo.html"));
+        });
+        
+        app.use((req, res) => {
+            res.status(404).send("Page Not Found");
+        });
+
+
+        app.listen(HTTP_PORT, () => {
+            console.log("Server listening on port: " + HTTP_PORT);
+        });
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+
+    app.get('/theme.css', (req, res)=>{
+        res.sendFile(path.join(__dirname, "/css/theme.css"));
+    });
